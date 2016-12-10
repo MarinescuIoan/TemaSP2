cb64d86d8aebae92d7929ca1b40bac6d1b46ec5f
MarinescuIoan <marinescuioan04@gmail.com>
2016-12-10 17:20:32 +0200

    Update Tema2_MarinescuIoan_421C.m

--- Tema2_MarinescuIoan_421C.m
+++ Tema2_MarinescuIoan_421C.m
@@ -1,7 +1,7 @@
-%Tema nr. 2 - Semnale si Programare - numarul din lista = 13, deci vom avea
-% un semnal dreptugnhiular cu perioada 40 si N = 50 si durata = 13.
+%Tema 2 - Semnale si Programare - numarul din lista = 15, deci vom avea
+% un semnal dreptugnhiular cu perioada 40 si N = 50 si durata = 15.
 
-D=13;
+D=15;
 N=50;
 P=40;
 F=1/P;
@@ -11,50 +11,49 @@ w0=2*pi*F;
 %generez un vector cu zerouri:
 x = zeros(1,size(t,2));
 
-%aranjarea vectorului
+%construirea vectorului
 x(t<=D)=1;
 x(t>D)=-1;
 x(t>P-D)=1;
 
-%seria
+% determinare coeficienti
 for k = -N:N
     xt = x;
     xt = xt.*exp(-j*k*w0*t);
     X(k+51) = trapz(t,xt); 
 end
 
-%reconstruirea semnalului (aproximare)
+%reconstruirea semnalului dat
 
-x_aprox(1:length(t)) = 0; 
+x_nounout(1:length(t)) = 0; 
 
 for const = 1:length(t);
 for k = -N:N
-x_aprox(const) = x_aprox(const) + (1/P)*X(k+N+1)*exp(j*k*w0*t(const));
+x_nounout(const) = x_nounout(const) + (1/P)*X(k+N+1)*exp(j*k*w0*t(const));
 end
 end % semnalul dat cu coeficientii seriei
 
 figure(1);
 plot(t,x); % afisez mai intai x(t)
 
-title('x(t) si semnalul alcatuit prin esantionare');
+title('x(t) si armonicile semnalului esantionat');
 hold on
 
-plot(t,x_aprox,'r'); 
+plot(t,x_nounout,'g'); 
 
 xlabel('Timp (s)');
 ylabel('Amplitudine');
 
 
-fr = -N*F:F:N*F; % vectorul de frecvente pentru afisarea semnalului
+frecventa = -N*F:F:N*F; % vectorul de frecvente 
 figure(2);
-stem(fr,abs(X)); 
+stem(frecventa,abs(X)); 
 title('Spectrul x(t)');
 xlabel('Frecventa (Hz)');
 ylabel('|X|');
 
-%Seria Fourier este reprezentarea semnalului sub forma de suma pana la
-%infinit din: sinusuri si cosinusuri (SFT),exponentiale (SFE), sau armonici
-%(SFA). Din cursuri am observat ca semnalul poate fi analizat pe esantioane
-%si aproximat astfel incat sa obtinem un model cat mai apropiat de cel real
-%si o eroare cat mai mica. Astfel, am esantionat si am calculat
-%coeficientii seriei Fourier.
+% Am folosit informatiile din curs despre seriile Fourier ca sume infinite de sinusuri si
+% cosinusuri pentru SFT, armonici pentru SFA si exponentiale pentru SFE. Aceste sume pot 
+% fi aproximate prin esantionare, pentru a putea fi mai  usor de analizat si fara a produce 
+% prea mari erori. Asadar, am reconstruit semnalul cu ajutorul formulelor de la Semnale si
+%Sisteme.
